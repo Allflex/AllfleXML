f0184473330a339bd36217ac69bae4f07ec2f212
Timothy Sturm <tsturm@allflexusa.com>
2017-05-08 18:04:48 -0500

    Ouch, you hurt my toes Andrew

--- AllfleXML/FlexOrder/FlexOrder.cs
+++ AllfleXML/FlexOrder/FlexOrder.cs
@@ -114,6 +114,9 @@ namespace AllfleXML.FlexOrder
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     [XmlRoot]
     public class Document
@@ -122,127 +125,417 @@ namespace AllfleXML.FlexOrder
         public List<OrderHeader> OrderHeaders { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class OrderHeader
     {
+        /// <summary>
+        /// Gets or sets the Customer account number.
+        /// </summary>
+        /// <value>
+        /// The customer number.
+        /// </value>
         [XmlElement("CustomerNumber", IsNullable = false)]
         public string CustomerNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Premise Identification Code or the LID number.
+        /// </summary>
+        /// <value>
+        /// The Premise Idenfication Code (PIC) or the Location Identification Number (LID).
+        /// </value>
         [XmlElement("PremiseID")]
         public string PremiseID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comments.
+        /// </summary>
+        /// <value>
+        /// The comments.
+        /// </value>
         [XmlElement("Comments")]
         public string Comments { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Purchase Order number.
+        /// </summary>
+        /// <value>
+        /// The po.
+        /// </value>
         [XmlElement("PO", IsNullable = false)]
         public string PO { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The name of the ship to.
+        /// </value>
         [XmlElement("ShipToName", IsNullable = false)]
         public string ShipToName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact at the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to contact.
+        /// </value>
         [XmlElement("ShipToContact")]
         [Obsolete("This field is deprecated. Please use ShipToName instead.")]
         public string ShipToContact { get; set; } // TODO: Remove from specification
+
+        /// <summary>
+        /// Gets or sets the phone number of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to phone.
+        /// </value>
         [XmlElement("ShipToPhone")]
         public string ShipToPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first address line of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to address1.
+        /// </value>
         [XmlElement("ShipToAddress1", IsNullable = false)]
         public string ShipToAddress1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second address line of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to address2.
+        /// </value>
         [XmlElement("ShipToAddress2")]
         public string ShipToAddress2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the third address line of the shipping address
+        /// </summary>
+        /// <value>
+        /// The ship to address3.
+        /// </value>
         [XmlElement("ShipToAddress3")]
         public string ShipToAddress3 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to city.
+        /// </value>
         [XmlElement("ShipToCity", IsNullable = false)]
         public string ShipToCity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The state of the ship to.
+        /// </value>
         [XmlElement("ShipToState", IsNullable = false)]
         public string ShipToState { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postal code of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to postal code.
+        /// </value>
         [XmlElement("ShipToPostalCode", IsNullable = false)]
         public string ShipToPostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country of the shipping address.
+        /// </summary>
+        /// <value>
+        /// The ship to country.
+        /// </value>
         [XmlElement("ShipToCountry")]
         public string ShipToCountry { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this order is a rush.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this order is a rush; otherwise, <c>false</c>.
+        /// </value>
         [XmlElement("IsRush")]
         public bool IsRush { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shipping method.
+        /// </summary>
+        /// <value>
+        /// The shipping method.
+        /// </value>
         [XmlElement("ShipMethod")]
         public string ShipMethod { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shipping account number.
+        /// </summary>
+        /// <value>
+        /// The shipping account number.
+        /// </value>
         [XmlElement("ShippingAccountNumber")]
         public string ShippingAccountNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email list error.
+        /// </summary>
+        /// <value>
+        /// The email list error.
+        /// </value>
         [XmlElement("EmailListError")]
         public string EmailListError { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email list tracking information.
+        /// </summary>
+        /// <value>
+        /// The email list tracking information.
+        /// </value>
         [XmlElement("EmailListTrackingInfo")]
         public string EmailListTrackingInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email list eid information.
+        /// </summary>
+        /// <value>
+        /// The email list eid information.
+        /// </value>
         [XmlElement("EmailListEIDInfo")]
         public string EmailListEIDInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user defined fields.
+        /// </summary>
+        /// <value>
+        /// The user defined fields.
+        /// </value>
         [XmlElement("UserDefined")]
         public UserDefinedFields UserDefinedFields { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ws order identifier.
+        /// </summary>
+        /// <value>
+        /// The ws order identifier.
+        /// </value>
         [XmlElement("WSOrderId")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public string WSOrderId { get; set; } // TODO: Remove from specification
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [order hold].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [order hold]; otherwise, <c>false</c>.
+        /// </value>
         [XmlElement("Hold")]
         public bool OrderHold { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order line headers.
+        /// </summary>
+        /// <value>
+        /// The order line headers.
+        /// </value>
         [XmlElement("OrderLineHeader")]
         public List<OrderLineHeader> OrderLineHeaders { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class UserDefinedFields
     {
+        /// <summary>
+        /// Gets or sets the fields.
+        /// </summary>
+        /// <value>
+        /// The fields.
+        /// </value>
         [XmlElement("Field")]
         public List<UserDefinedField> Fields { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class UserDefinedField
     {
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
         [XmlAttribute("Key")]
         public string Key { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         [XmlText]
         public string Value { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class OrderLineHeader
     {
+        /// <summary>
+        /// Gets or sets the name of the sku.
+        /// </summary>
+        /// <value>
+        /// The name of the sku.
+        /// </value>
         [XmlElement("SkuName", IsNullable = false)]
         public string SkuName { get; set; }
+        /// <summary>
+        /// Gets or sets the premise identifier.
+        /// </summary>
+        /// <value>
+        /// The premise identifier.
+        /// </value>
         [XmlElement("PremiseID")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public string PremiseID { get; set; } // TODO: Remove from specification
+        /// <summary>
+        /// Gets or sets the quantity.
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
         [XmlElement("Quantity", IsNullable = false)]
         public int Quantity { get; set; }
+        /// <summary>
+        /// Gets or sets the template.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
         [XmlElement("Template")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public string Template { get; set; } // TODO: Remove from specification
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
+        /// <value>
+        /// The comment.
+        /// </value>
         [XmlElement("Comment")]
         public string Comment { get; set; }
+        /// <summary>
+        /// Gets or sets the user defined fields.
+        /// </summary>
+        /// <value>
+        /// The user defined fields.
+        /// </value>
         [XmlElement("UserDefined")]
         public UserDefinedFields UserDefinedFields { get; set; }
+        /// <summary>
+        /// Gets or sets the order line custom.
+        /// </summary>
+        /// <value>
+        /// The order line custom.
+        /// </value>
         [XmlElement("OrderLineCustom")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public List<OrderLineCustom> OrderLineCustom { get; set; } // TODO: Remove from specification
+        /// <summary>
+        /// Gets or sets the order line marking detail.
+        /// </summary>
+        /// <value>
+        /// The order line marking detail.
+        /// </value>
         [XmlElement("OrderLineMarkingDetail")]
         public List<OrderLineMarkingDetail> OrderLineMarkingDetail { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class OrderLineMarkingDetail
     {
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
+        /// <value>
+        /// The comment.
+        /// </value>
         [XmlElement("Comment")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public string Comment { get; set; } // TODO: Remove from specification
+        /// <summary>
+        /// Gets or sets the variables.
+        /// </summary>
+        /// <value>
+        /// The variables.
+        /// </value>
         [XmlElement("Variable")]
         public List<Variable> Variables { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     [Obsolete("This class is deprecated. It will be removed in the next version.")]
     public class OrderLineCustom // TODO: Remove from specification
     {
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
+        /// <value>
+        /// The comment.
+        /// </value>
         [XmlElement("Comment")]
         [Obsolete("This field is deprecated. It will be removed in the next version.")]
         public string Comment { get; set; } // TODO: Remove from specification
+        /// <summary>
+        /// Gets or sets the variables.
+        /// </summary>
+        /// <value>
+        /// The variables.
+        /// </value>
         [XmlElement("Variable")]
         public List<Variable> Variables { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Serializable]
     public class Variable
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         [XmlElement("VariableName")]
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         [XmlElement("VariableValue")]
         public string Value { get; set; }
     }
--- AllfleXML/FlexOrderStatus/FlexOrderStatus.cs
+++ AllfleXML/FlexOrderStatus/FlexOrderStatus.cs
@@ -91,84 +91,433 @@ namespace AllfleXML.FlexOrderStatus
     [XmlRoot]
     public class OrderStatus
     {
-        [XmlElement("Guid")]
-        public string Guid { get; set; }
+        /// <summary>
+        /// Unique identifier of an order.
+        /// </summary>
+        /// <value>
+        /// The API Global Unique Identifier
+        /// </value>
+        [XmlElement("WSOrderId", IsNullable = false)]
+        public string WSOrderId { get; set; }
 
-        [XmlElement("PurchaseOrderNumber")]
-        public string PurchaseOrderNumber { get; set; }
+        /// <summary>
+        /// Gets or sets the Purchase Order number.
+        /// </summary>
+        /// <value>
+        /// The po.
+        /// </value>
+        [XmlElement("PO", IsNullable = false)]
+        public string PO { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Master Identification number. An intenal Reference number
+        /// </summary>
+        /// <value>
+        /// The master id (internal to Allflex)
+        /// </value>
         [XmlElement("MasterId")]
-        public int MasterId { get; set; }
+        public int MasterId { get; set; } = -1;
+        
+        [XmlIgnore]
+        public bool MasterIdSpecified => MasterId >= 0;
 
-        [XmlElement("AllflexOrderId")]
-        public string AllflexOrderId { get; set; }
+        /// <summary>
+        /// Gets or sets the Order Idenfication number. An internal Reference number
+        /// </summary>
+        /// <value>
+        /// The allflex order identifier.
+        /// </value>
+        [XmlElement("OrderId")]
+        public string OrderId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the status of the order.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
         [XmlElement("Status")]
         public string Status { get; set; }
 
-        [XmlElement("ProductionProgress")]
-        public int ProductionProgress { get; set; }
+        /// <summary>
+        /// Gets or sets the progress percentage of the order.
+        /// </summary>
+        /// <value>
+        /// The progress percentage (in Int form)
+        /// </value>
+        [XmlElement("Progress")]
+        public int Progress { get; set; } = -1;
 
-        [XmlElement("Shipping")]
-        public Shipping Shipping { get; set; }
+        [XmlIgnore]
+        public bool ProgressSpecified => Progress >= 0;
+
+        /// <summary>
+        /// Gets or sets a list of Shipments.
+        /// </summary>
+        /// <value>
+        /// List of Shipments for the order.
+        /// </value>
+        [XmlElement("Shipment")]
+        public List<Shipment> Shipment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the messages for an order.
+        /// </summary>
+        /// <value>
+        /// Order Messages.  
+        /// </value>
+        [XmlElement("Messages")]
+        public List<Message> Messages { get; set; }
+    }
+
+    [Serializable]
+    public class Message
+    {
+        /// <summary>
+        /// Gets or sets the actual error message.
+        /// </summary>
+        /// <value>
+        /// The particular error message set by the system. 
+        /// </value>
         [XmlElement("ErrorMessage")]
-        public List<ErrorMessage> ErrorMessages { get; set; }
+        public string ErrorMessage { get; set; }
     }
 
+    /// <summary>
+    /// The Shipment information for the order
+    /// </summary>
+    /// <remarks>
+    /// Shipment info will be set at order entry, but fields like Tracking Number and Freight Amount will be applied at Dispatch and Invoicing, respectively. 
+    /// </remarks>
     [Serializable]
-    public class ErrorMessage
+    public class Shipment
     {
-        [XmlElement("Message")]
-        public string Message { get; set; }
-    }
-
-    [Serializable]
-    public class Shipping
-    {
+        /// <summary>
+        /// Gets or sets the ship method.
+        /// </summary>
+        /// <value>
+        /// The shipping method for the given order. 
+        /// </value>
         [XmlElement("ShipMethod")]
         public string ShipMethod { get; set; }
 
+        /// <summary>
+        /// Gets or sets the shipping account number.
+        /// </summary>
+        /// <value>
+        /// The shipping account number.
+        /// </value>
         [XmlElement("ShippingAccountNumber")]
         public string ShippingAccountNumber { get; set; }
 
+        /// <summary>
+        /// Gets or sets the freight amount.
+        /// </summary>
+        /// <value>
+        /// The freight amount for the order. 
+        /// </value>
         [XmlElement("FreightAmount")]
-        public double FreightAmount { get; set; }
+        public double FreightAmount { get; set; } = -1;
 
+        [XmlIgnore]
+        public bool FreightAmountSpecified => FreightAmount >= 0;
+
+        /// <summary>
+        /// Gets or sets the tracking number.
+        /// </summary>
+        /// <value>
+        /// The tracking number give by the shipping company. 
+        /// </value>
         [XmlElement("TrackingNumber")]
         public string TrackingNumber { get; set; }
 
+        /// <summary>
+        /// Gets or sets the shipping date.
+        /// </summary>
+        /// <value>
+        /// The shipping date for the order. 
+        /// </value>
         [XmlElement("ShippingDate")]
-        public DateTime ShippingDate { get; set; }
+        public DateTime? ShippingDate { get; set; }
 
+        [XmlIgnore]
+        public bool ShippingDateSpecified => ShippingDate != null;
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>
+        /// The shipping address provided by the customer. 
+        /// </value>
         [XmlElement("Address")]
-        public ShippingAddress Address { get; set; }
+        public Address Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order lines.
+        /// </summary>
+        /// <value>
+        /// The order lines for the given order.
+        /// </value>
+        [XmlElement("OrderLine")]
+        public List<OrderLine> OrderLines { get; set; }
     }
 
     [Serializable]
-    public class ShippingAddress
+    public class Address
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name to the person or business that the order is being shipped to. 
+        /// </value>
         [XmlElement("Name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Name of the recipient of the order. Unused, please use Name.
+        /// </summary>
+        [XmlElement("Contact")]
+        [Obsolete("This field is deprecated. It will be removed in the next version.")]
+        public string Contact { get; set; }
+
+        /// <summary>
+        /// Telephone number where the addressed can be reached.
+        /// </summary>
+        [XmlElement("Phone")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first address line.
+        /// </summary>
+        /// <value>
+        /// The first address line for the order. 
+        /// </value>
         [XmlElement("Address1")]
         public string Address1 { get; set; }
 
+        /// <summary>
+        /// Gets or sets the second address line.
+        /// </summary>
+        /// <value>
+        /// The second address line for the order (if needed)
+        /// </value>
         [XmlElement("Address2")]
         public string Address2 { get; set; }
 
+        /// <summary>
+        /// Gets or sets the third address line.
+        /// </summary>
+        /// <value>
+        /// The third adress line for the order (if needed).
+        /// </value>
         [XmlElement("Address3")]
         public string Address3 { get; set; }
 
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The the city/town. 
+        /// </value>
         [XmlElement("City")]
         public string City { get; set; }
 
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>
+        /// The state/province.
+        /// </value>
         [XmlElement("State")]
         public string State { get; set; }
 
+        /// <summary>
+        /// Gets or sets the postal code.
+        /// </summary>
+        /// <value>
+        /// The postal code.
+        /// </value>
         [XmlElement("PostalCode")]
         public string PostalCode { get; set; }
 
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>
+        /// The country
+        /// </value>
         [XmlElement("Country")]
         public string Country { get; set; }
+    }
+
+    [Serializable]
+    public class OrderLine
+    {
+        /// <summary>
+        /// Gets or sets the item number.
+        /// </summary>
+        /// <value>
+        /// The item number or sku for the order line. 
+        /// </value>
+        [XmlElement("ItemNumber")]
+        public string ItemNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quanitity.
+        /// </summary>
+        /// <value>
+        /// The amount of items being ordered. 
+        /// </value>
+        [XmlElement("Quantity")]
+        public int Quanitity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the template.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        [XmlElement("Template")]
+        public string Template { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the order line.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
+        [XmlElement("Status")]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the progress percentage of the order line.
+        /// </summary>
+        /// <value>
+        /// The progress percentage (in Int form)
+        /// </value>
+        [XmlElement("Progress")]
+        public int Progress { get; set; } = -1;
+
+        [XmlIgnore]
+        public bool ProgressSpecified => Progress >= 0;
+
+        /// <summary>
+        /// Gets or sets the tag manifest.
+        /// </summary>
+        /// <value>
+        /// The tag manifest (if applicable). 
+        /// </value>
+        [XmlElement("TagManifest")]
+        public TagManifest TagManifest { get; set; }
+    }
+    
+    [Serializable]
+    public class TagManifest
+    {
+        /// <summary>
+        /// Gets or sets the packaging containers.
+        /// </summary>
+        /// <value>
+        /// The packaging containers.
+        /// </value>
+        [XmlElement("Container")]
+        public List<Container> Containers { get; set; }
+    }
+
+    [Serializable]
+    public class Container
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [XmlElement("ID")]
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        [XmlElement("Type")]
+        public string Type { get; set; }
+
+        [XmlElement("Tags")]
+        public Tags Tags { get; set; }
+
+        /// <summary>
+        /// Gets or sets the child container.
+        /// </summary>
+        /// <value>
+        /// The child container.
+        /// </value>
+        [XmlElement("Container")]
+        public List<Container> ChildContainers { get; set; }
+    }
+
+    [Serializable]
+    public class Tags
+    {
+        /// <summary>
+        /// Gets or sets the tag sets.
+        /// </summary>
+        /// <value>
+        /// The tag sets (of variables)
+        /// </value>
+        [XmlElement("TagSet")]
+        public List<TagSet> TagSets { get; set; }
+    }
+    
+    [Serializable]
+    public class TagSet
+    {
+        /// <summary>
+        /// Gets or sets the tag variables
+        /// </summary>
+        /// <value>
+        /// The variables for a given tag. 
+        /// </value>
+        [XmlElement("Variable")]
+        public List<Variable> Variables { get; set; }
+    }
+
+    [Serializable]
+    public class Variable
+    {
+        /// <summary>
+        /// Gets or sets the variable name.
+        /// </summary>
+        /// <value>
+        /// The variable name.  Could be "EID", "Management", "Serial", etc. 
+        /// </value>
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the variable value
+        /// </summary>
+        /// <value>
+        /// The value for the variable named with the "Name" field.  For instance, a variable named "EID" might have a value of "9821234567890".
+        /// </value>
+        [XmlElement("Value")]
+        public string Value { get; set; }
+    }
+    
+    public enum ContainerType
+    {
+        Pallet,
+        Case,
+        Bag
+    }
+
+    public enum ManifestType
+    {
+        Serial,
+        EID,
+        Management
     }
 }

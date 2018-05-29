f88be7231128472baf37f0f80b222ec2aa487df2
Timothy Sturm <tsturm@allflexusa.com>
2018-05-29 10:44:46 -0500

    regenerated

--- com.allflex.api/GeneratePOJOFromXSDusingXJC.bat
+++ com.allflex.api/GeneratePOJOFromXSDusingXJC.bat
@@ -1,3 +1,4 @@
-xjc -b com.allflex.api\binding.xml -d com.allflex.api\src -p com.allflex.api.flexorder AllfleXML\FlexOrder\FlexOrder.xsd
-xjc -b com.allflex.api\binding.xml -d com.allflex.api\src -p com.allflex.api.flexorderstatus AllfleXML\FlexOrderStatus\FlexOrderStatus.xsd
-xjc -b com.allflex.api\binding.xml -d com.allflex.api\src -p com.allflex.api.flexspec AllfleXML\FlexSpec\FlexSpec.xsd
+xjc -b binding.xml -d src -p com.allflex.api.flexorder ..\AllfleXML\FlexOrder\FlexOrder.xsd
+xjc -b binding.xml -d src -p com.allflex.api.flexorderstatus ..\AllfleXML\FlexOrderStatus\FlexOrderStatus.xsd
+xjc -b binding.xml -d src -p com.allflex.api.flexspec ..\AllfleXML\FlexSpec\FlexSpec.xsd
+pause

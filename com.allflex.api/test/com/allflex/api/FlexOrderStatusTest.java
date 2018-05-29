a864c50519f93c993a49644a82db34641f9228d9
Timothy Sturm <tsturm@allflexusa.com>
2018-05-29 12:10:10 -0500

    Created unit tests

---
+++ com.allflex.api/test/com/allflex/api/FlexOrderStatusTest.java
@@ -0,0 +1,37 @@
+package com.allflex.api;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlexOrderStatusTest {
+    
+    public FlexOrderStatusTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
